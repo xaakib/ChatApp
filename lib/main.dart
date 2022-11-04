819d997a44179d83b75e12848de487b94c81c889
Shakib Hossen <xaakibx@gmail.com>
2022-11-04 12:46:17 +0600

    check

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //check
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Chat App',
